0df4ad6c8df2da5485e0f07ee4aa592e05d172e3
RomanR_ASUS <roman.r.sbs@gmail.com>
2025-09-22 15:19:39 +0300

    [dev] add mapping

---
+++ Notes/Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace Notes.Application.Common.Mappings;
+
+public interface IMapWith<T>
+{
+     void Mapping(Profile profile)
+     {
+         profile.CreateMap(typeof(T), GetType());
+     }
+}
