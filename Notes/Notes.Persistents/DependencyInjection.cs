1369dd32902e22112e8c6bcceb842792ad0e524f
RomanR_ASUS <roman.r.sbs@gmail.com>
2025-09-27 12:49:21 +0300

    [dev] add validation

---
+++ Notes/Notes.Persistents/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.interfaces;
+
+namespace Notes.Persistents;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection service,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+        service.AddDbContext<NotesDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        service.AddScoped<INotesDbContext>(provider =>
+            provider.GetService<NotesDbContext>());
+
+        return service;
+    }
+}
---
+++ Notes/Notes.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notes.WebApi.Controllers;
+
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    private IMediator _mediator;
+
+    protected IMediator Mediator()
+    {
+        return _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+
+    internal Guid UserId()
+    {
+        return !User.Identity.IsAuthenticated
+            ? Guid.Empty
+            : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+    }
+}
